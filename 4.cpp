e39a02a269df1b3ec1a1192c0155b197ff1f37f9
valeria <leradopira@mail.ru>
2019-12-25 00:35:26 +0300

    Lab 4

--- 4.cpp
+++ 4.cpp
@@ -1,287 +1,222 @@
-﻿#include <iostream>
-#include <array>
-#include <vector>
-#include <numeric>
-#include <random>
-#include <algorithm>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "mpi.h"
 
-#define DataType MPI_INT
-#define ControlType MPI_UNSIGNED
+int main(int argc, char **argv) {
+	int ProcNum, ProcRank;
+	MPI_Status Status;
+	MPI_Request request;
 
-using namespace std;
-
-/*
-* Id процесса с рангом 0
-*/
-constexpr int ROOT_RANK = 0;
-
-/*
-* Идентификаторы сообщений для MPI_Send и MPI_Recv
-* Идентификатор от 0 - 1, сообщает процессу что он получил, команду или данные
-*/
-
-enum class Tags : int { // Перечисление
-	CONTROL,
-	DATA,
-};
-
-/*
-* Управляющие команды/сигналы для дочерних процессов, что работают с FIFO
-*/
-
-enum class Command : unsigned {  // Перечисление
-	PUSH, // Положить в буфер
-	GET, // Получить из буфера
-	POP, // Удалить из буфера
-	STOP, // Остановить работу с буфером. Завершить работу цикла
-};
-
-using value_type = int;
-//constexpr MPI_Datatype DataType = MPI_INT; // Ссылки на типы из библиотеки MPI
-//constexpr MPI_Datatype ControlType = MPI_UNSIGNED; // Ссылки на типы из библиотеки MPI
-
-/*
-* Реализация FIFO по типу очереди
-* Первый вошёл – первый вышел
-*/
-
-class MPIQueue {
-
-
-	unsigned it_ = 0; // С какого места начинаем писать в очередь
-	unsigned size_ = 0; // Размер уже записанного
-	const unsigned capacity_; // Размер FIFO. Зависит от количества процессов или процессоров
-
-public:
-
-	/*
-	*
-	* Конструктор класса.
-	* Получает от процесса 0, общее количество процессов
-	*/
-	MPIQueue(unsigned capacity) : capacity_(capacity) {}
-
-	/*
-	* Послать всем дочерним процессам сигнал записи и данные
-	*/
-	bool push(const value_type &val)
-	{
-		if (size_ == capacity_)
-			return false;
-		
-		unsigned pos = (it_ + size_) % capacity_;
-
-		Command cmd(Command::PUSH);
-		MPI_Send(&cmd, 1, ControlType, pos + 1, int(Tags::CONTROL), MPI_COMM_WORLD);
-		MPI_Send(&val, 1, DataType, pos + 1, int(Tags::DATA), MPI_COMM_WORLD);
-
-		++size_;
-		return true;
-	}
-
-	/*
-	* Послать всем дочерним процессам сигнал на получение данных
-	* Собёрем что у них есть
-	*/
-	bool get(value_type &val) const
-	{
-		if (size_ == 0)
-			return false;
-
-		Command cmd(Command::GET);
-		MPI_Send(&cmd, 1, ControlType, it_ + 1, int(Tags::CONTROL), MPI_COMM_WORLD);
-		MPI_Recv(&val, 1, DataType, it_ + 1, int(Tags::DATA), MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		return true;
-	}
-
-	/*
-	* Послать всем дочерним процессам сигнал на удаление данных из FIFO
-	*/
-	bool pop()
-	{
-		if (size_ == 0)
-			return false;
-
-
-		Command cmd(Command::POP);
-		MPI_Send(&cmd, 1, ControlType, it_ + 1, int(Tags::CONTROL), MPI_COMM_WORLD);
-
-		it_ = (it_ + 1) % capacity_;
-		--size_;
-		return true;
-	}
-
-};
-
-/*
-* Процесс с рангом 0
-* Управляет всеми остальными процессами
-*/
-
-void master(int argc, char* argv[])
-{
-	// push to max, pop to 0, check data
-
-	/*
-	for (;;) {
-		char cmd;
-		std::cin >> cmd;
-		switch (cmd) {
-		case '+':
-		case '-':
-		case '=':
-		case 'q':
-			return;
-		default:
-			std::cout << "unexpected command\n";
-		};
-	}
-	*/
-
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size); // Функция определения числа процессов в области связи MPI_Comm_size
-
-	--size; // Минус процесс с рангом 0
-
-	MPIQueue queue(size); // Обёртка FIFO
-
-	value_type val = 0;
-
-	/*
-	* Тест
-	* 
-	* Неудачная попытка получить значение из пустого FIFO
-	*/
-	if (queue.pop() || queue.get(val)) {
-		std::cerr << "get from empty queue";
-		return;
-	}
-
-
-	while (queue.push(val++)) {} // Заполним буфер int
-
-	for (unsigned i = 0; i < size; ++i) {
-
-		/*
-		* Тест 2
-		*
-		* Попытка получить значение из полного FIFO
-		*/
-
-		if (!queue.get(val) || !queue.pop()) {
-			std::cerr << "get from full queue";
-			return;
-		}
-
-		/*
-		* Тест 3
-		*
-		* Полученный результат не соответствует ожиданию
-		*/
-
-		if (i != val) {
-			std::cerr << "get wrong: " << val << "expected " << i;
-			return;
-		}
-	}
-}
-
-/*
-* Процессы с рангом > 0
-*/
-void slave(MPI_Status& status)
-{
-	value_type storage;
-
-	bool full = false; // Флаг если очередь полна
-
-	/*
-	* Циклический буфер
-	*/
-	for (;;) {
-		Command cmd;
-
-		/*
-		* Процесс ожидает сигнал от мастера на прерывание цикла
-		*/
-		if (MPI_Recv(&cmd, 1, ControlType, ROOT_RANK, int(Tags::CONTROL), MPI_COMM_WORLD, &status)) {
-			return;
-		}
-
-		switch (cmd) {
-
-			/*
-			* Получить данные от мастера и поместить их в очередь
-			*/
-			case Command::PUSH:
-				/* 
-				* Тест 4
-				* Что-то сделать если очередь заполнилась
-				*/
-				if (full) {
-					// this is controlled in stack
-				}
-				full = true;
-				if (MPI_Recv(&storage, 1, DataType, ROOT_RANK, int(Tags::DATA), MPI_COMM_WORLD, &status))
-					return;
-				std::cout << "push " << storage << std::endl;
-				break;
-
-			/*
-			* Отправить данные мастеру
-			*/
-			case Command::GET:
-				if (MPI_Send(&storage, 1, DataType, ROOT_RANK, int(Tags::DATA), MPI_COMM_WORLD)) {
-					return;
-				}
-				std::cout << "get " << storage << std::endl;
-				break;
-
-			/*
-			* Очистить очередь
-			*/
-			case Command::POP:
-				/*
-				* Тест 5
-				* Что-то сделать если очередь пуста
-				*/
-				if(!full) {
-					// this is controlled in stack
-				}
-				full = false;
-				std::cout << "pop " << storage << std::endl;
-				break;
-
-			case Command::STOP:
-				return;
-		}
-	}
-}
-int main(int argc, char* argv[]) {
-	int rank;
-	MPI_Status status;
+	//Tags:
+	int tag_push = 0;
+	int tag_element_pushed = 1;
+	int tag_pop = 2;
+	int tag_element_poped = 3;
+	int tag_full = 4;
+	int tag_empty = 5;
+	int tag_end = 6;
+	int tag_wait_to_push = 7;
+	int tag_wait_to_pop = 8;
+	int tag_not_wait_to_push = 9;
+	int tag_not_wait_to_pop = 10;
 
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int NOTHING;
 
-	// Если это процесс с 0 рангом
-	if (rank == 0) {
-		master(argc, argv);
+	if (ProcRank == 0) 
+	{
+		int Size = 0;
+		printf("Please, select command \n Enter 1 to push element\n Enter 2 to pop element\n Enter 3 to close program\n");
 
-		Command cmd(Command::STOP);
-		for(int i = 1; i < size; ++i)
-			MPI_Send(&cmd, 1, ControlType, i, int(Tags::CONTROL), MPI_COMM_WORLD);
+		for (;;) {
+			int command;
+			scanf("%i", &command);
 
-	} else {
-		slave(status);
+			if (command == 1) 
+			{
+				int Element;
+				scanf("%i", &Element);
+
+				MPI_Send(&Element, 1, MPI_INT, 1, tag_push, MPI_COMM_WORLD);
+				MPI_Recv(&NOTHING, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+
+				if (Status.MPI_TAG == tag_element_pushed) 
+				{
+					Size++;
+					printf("%i has been pushed\n", Element);
+					// printf("Size of Queue: %i\n", Size);
+				}
+				else 
+				{
+					printf("Queue is full\n");
+				}
+
+			} 
+			else if (command == 2) 
+			{
+				int RecvElement;
+				MPI_Send(&NOTHING, 1, MPI_INT, ProcNum - 1, tag_pop, MPI_COMM_WORLD);
+				MPI_Recv(&RecvElement, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+
+				if (Status.MPI_TAG == tag_element_poped) 
+				{
+					Size--;
+					printf("%i has been popped\n", RecvElement);
+					// printf("Size of Queue: %i\n", Size);
+				}
+				else 
+				{
+					printf("Fail! Queue is empty.\n");
+				}
+
+			} 
+			else if (command == 3) 
+			{
+				for (int i = 1; i < ProcNum; i++) 
+				{
+					MPI_Send(&NOTHING, 1, MPI_INT, i, tag_end, MPI_COMM_WORLD);
+				}
+				MPI_Finalize();
+				return 0;
+			}
+			else 
+			{
+				printf("Wrong input! Try again\n");
+			}
+
+		}
+	} 
+
+	else
+	{
+		int containsElement = false;
+		int is_wait_to_push;
+		int is_wait_to_pop = false;
+
+		if (ProcRank == ProcNum - 1) 
+		{
+			is_wait_to_push = true;
+		}
+		else
+		{
+			is_wait_to_push = false;
+		}
+
+		int Element;
+		int RecvElement;
+
+		for (;;) 
+		{
+			MPI_Recv(&RecvElement, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+
+			if (Status.MPI_TAG == tag_push) 
+			{
+				if (containsElement) 
+				{
+					MPI_Send(&NOTHING, 1, MPI_INT, 0, tag_full, MPI_COMM_WORLD);
+				} 
+
+				else if (is_wait_to_push) 
+				{
+					Element = RecvElement;
+					containsElement = true;
+					is_wait_to_push = false;
+					is_wait_to_pop = true;
+
+					if (ProcRank == 1) {
+						MPI_Send(&Element, 1, MPI_INT, 0, tag_element_pushed, MPI_COMM_WORLD);
+					} 
+					else
+					{
+						MPI_Send(&Element, 1, MPI_INT, ProcRank - 1, tag_wait_to_push, MPI_COMM_WORLD);
+					}
+
+					if (ProcRank != ProcNum - 1) 
+					{
+						MPI_Send(&NOTHING, 1, MPI_INT, ProcRank + 1, tag_not_wait_to_pop, MPI_COMM_WORLD);
+					}
+
+				} 
+
+				else 
+				{
+					MPI_Send(&RecvElement, 1, MPI_INT, ProcRank + 1, tag_push, MPI_COMM_WORLD);
+				}
+
+			} 
+			else if (Status.MPI_TAG == tag_wait_to_push) 
+			{
+				if (Status.MPI_TAG == tag_wait_to_push) 
+				{
+					is_wait_to_push = true;
+				}
+				MPI_Send(&RecvElement, 1, MPI_INT, ProcRank - 1, tag_element_pushed, MPI_COMM_WORLD);
+			} 
+			else if (Status.MPI_TAG == tag_not_wait_to_push) 
+			{
+				is_wait_to_push = false;
+			} 
+			else if (Status.MPI_TAG == tag_element_pushed) 
+			{
+				MPI_Send(&Element, 1, MPI_INT, ProcRank - 1, tag_element_pushed, MPI_COMM_WORLD);
+			}
+			else if (Status.MPI_TAG == tag_pop)
+			{
+				if (!containsElement) 
+				{
+					MPI_Send(&NOTHING, 1, MPI_INT, 0, tag_empty, MPI_COMM_WORLD);
+				}
+				else if (is_wait_to_pop) 
+				{
+					containsElement = false;
+					is_wait_to_push = true;
+					is_wait_to_pop = false;
+					if(ProcRank == ProcNum - 1) 
+					{
+						MPI_Send(&Element, 1, MPI_INT, 0, tag_element_poped, MPI_COMM_WORLD);
+					}
+					else
+					{
+						MPI_Send(&Element, 1, MPI_INT, ProcRank + 1, tag_wait_to_pop, MPI_COMM_WORLD);
+					}
+
+					if (ProcRank != 1) 
+					{
+						MPI_Send(&NOTHING, 1, MPI_INT, ProcRank - 1, tag_not_wait_to_push, MPI_COMM_WORLD);
+					}
+
+				} 
+				else 
+				{
+					MPI_Send(&NOTHING, 1, MPI_INT, ProcRank - 1, tag_pop, MPI_COMM_WORLD);
+				}
+			} 
+
+			else if (Status.MPI_TAG == tag_wait_to_pop) 
+			{
+				is_wait_to_pop = true;
+				MPI_Send(&Element, 1, MPI_INT, (ProcRank + 1) % ProcNum, tag_element_poped, MPI_COMM_WORLD);
+				Element = RecvElement;
+			} 
+			
+			else if (Status.MPI_TAG == tag_not_wait_to_pop) 
+			{
+				is_wait_to_pop = false;
+			} 
+			else if (Status.MPI_TAG == tag_element_poped) 
+			{
+				MPI_Send(&Element, 1, MPI_INT, (ProcRank + 1) % ProcNum, tag_element_poped, MPI_COMM_WORLD);
+				Element = RecvElement;
+			} 
+			else
+			{
+				MPI_Finalize();
+				return 0;
+			}
+		}
 	}
-
-	MPI_Finalize();
-
 	return 0;
 }
